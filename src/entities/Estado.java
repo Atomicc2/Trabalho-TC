fed51bfac29a967d5e72f1a823d93a21850faead
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-06-23 02:35:11 -0300

    Implementa a classe Estado

--- src/entities/Estado.java
+++ src/entities/Estado.java
@@ -1,4 +1,47 @@
 package entities;
 
 public class Estado {
+    private int id;
+    private String nome;
+    private boolean eInicial;
+    private boolean eFinal;
+
+    public Estado(String id, String nome, boolean eInicial, boolean eFinal) {
+        this.id = Integer.parseInt(id);
+        this.nome = nome;
+        this.eInicial = eInicial;
+        this.eFinal = eFinal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean iseInicial() {
+        return eInicial;
+    }
+
+    public void seteInicial(boolean eInicial) {
+        this.eInicial = eInicial;
+    }
+
+    public boolean iseFinal() {
+        return eFinal;
+    }
+
+    public void seteFinal(boolean eFinal) {
+        this.eFinal = eFinal;
+    }
 }
