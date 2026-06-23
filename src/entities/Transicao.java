67eb3c1eac5fc53d6af743382ce2a35a66172330
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-06-23 02:38:57 -0300

    Implementa a classe Transicao

--- src/entities/Transicao.java
+++ src/entities/Transicao.java
@@ -1,4 +1,36 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transicao {
+    private int de;
+    private int para;
+    private List<Integer> aoLer = new ArrayList<>();
+
+    public Transicao(int de, int para, List<Integer> aoLer) {
+        this.de = de;
+        this.para = para;
+        this.aoLer = aoLer;
+    }
+
+    public int getDe() {
+        return de;
+    }
+
+    public void setDe(int de) {
+        this.de = de;
+    }
+
+    public int getPara() {
+        return para;
+    }
+
+    public void setPara(int para) {
+        this.para = para;
+    }
+
+    public List<Integer> getAoLer() {
+        return aoLer;
+    }
 }
